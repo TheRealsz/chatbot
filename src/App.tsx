6ba14401912ea0d5b81e9d8e2a9ebe2938fecd7e
Robson Diego Andrade <robindiegoa@gmail.com>
2023-07-18 10:53:05 -0300

    Finalização da interação com a API e base do projeto completa

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,21 @@ import './index.css'
 import { AiOutlineSend } from 'react-icons/ai'
 import axios from 'axios';
 
-
-// P map + log
-// Lembrar de nao dar deploy devido ao limite de requisicoes?
 // Trabalhar com dark/light mode
 // Adicionar outro icone de robo
 // Adicionar nome proprio para o chatbot
-
 // Como evitar repetiçoes igual dos p?
 // Ver se vai ficar full ou assim mesmo no responsivo
+// Formatar response da API
+// P map
+// Aumentar o textarea enquanto o texto for adicionado
+// Lembrar de nao dar deploy devido ao limite de requisicoes?
 function App() {
 
 
   const [mensagem, setMensagem] = useState("")
-  const [mensagensDiv, setMensagensDiv] = useState<string[]>([]);
+  const [mensagensDiv, setMensagensDiv] = useState<{ type: 'user' | 'chatbot'; content: string }[]>([]);
   const [loading, setLoading] = useState(false)
-  const [resposta, setResposta] = useState<string[]>([]);
-  // const [view, setView] = useState(true)
 
 
 
@@ -47,57 +45,52 @@ function App() {
       )
       const data = response.data
       const messageAI = data.choices[0].message.content
-      setResposta([...resposta, messageAI])
+      setMensagensDiv((mensagemAnterior) => [...mensagemAnterior, { type: 'chatbot', content: messageAI }])
+      setLoading(false)
     } catch (error) {
-      setResposta([...resposta, "Ops, algo deu errado, tente novamente!"])
+      setMensagensDiv((mensagemAnterior) => [...mensagemAnterior, { type: 'chatbot', content: "Ops, algo deu errado, tente novamente!" }])
+      setLoading(false)
     }
   }
 
   function handleSendMensagem() {
-    setMensagensDiv([...mensagensDiv, mensagem])
+    setMensagensDiv((mensagemAnterior) => [...mensagemAnterior, { type: 'user', content: mensagem }])
     setMensagem('')
     setLoading(true)
-
-    setTimeout(() => {
-      setLoading(false)
-      // setView(true)
-    }, 1000)
-
     generateResponse()
   }
 
   return (
-    <div className='flex justify-center items-center h-screen bg-slate-950'>
-      <div className="bg-slate-900 text-brown-rust-100 w-105 rounded-xl shadow-specific overflow-hidden">
-        <header className='bg-gradient-to-r from-brown-rust-300 to-brown-rust-700 py-4 text-center'>
-          <h2 className='text-2xl font-bold'>Chatbot</h2>
-        </header>
-        <ul className="h-125 overflow-y-auto pt-4 pr-5 pb-16 text-base max-sm:pr-1">
-          <li className="flex">
-            <span className='h-8 w-8 text-center leading-8 mr-2 mb-2 self-end'>🤖</span>
-            <p className='bg-slate-300 text-slate-950 rounded-lg py-3 px-4 max-w-75'>Ola 😃 <br /> em que posso lhe ajudar?</p>
-          </li>
-          {mensagensDiv.map((mensagemDiv, index) => (
-            <li key={index} className="flex justify-end my-5">
-              <p className='bg-brown-rust-500 rounded-lg py-3 px-4'>{mensagemDiv}</p>
-            </li>
-          ))}
-          {loading && (
+    <div className='dark'>
+      <div className='flex justify-center items-center h-screen bg-slate-50 dark:bg-slate-950'>
+        <div className="bg-slate-100 dark:bg-slate-900 text-brown-rust-50 w-105 rounded-xl shadow-specific overflow-hidden">
+          <header className='bg-gradient-to-r from-brown-rust-300 to-brown-rust-700 py-4 text-center'>
+            <h2 className='text-2xl font-bold'>Chatbot</h2>
+          </header>
+          <ul className="h-125 overflow-y-auto pt-4 pr-5 pb-8 text-base max-sm:pr-1">
             <li className="flex">
               <span className='h-8 w-8 text-center leading-8 mr-2 mb-2 self-end'>🤖</span>
-              <p className='bg-slate-300 text-slate-950 rounded-lg py-3 px-4 max-w-75'>Pensando...</p>
+              <p className='bg-slate-300 text-slate-950 rounded-lg py-3 px-4 max-w-75'>Ola 😃 <br /> em que posso lhe ajudar?</p>
             </li>
-          )}
-          {resposta.map((resposta, index) => (
-            <li key={index} className="flex">
-              <span className='h-8 w-8 text-center leading-8 mr-2 mb-2 self-end'>🤖</span>
-              <p className='bg-slate-300 text-slate-950 rounded-lg py-3 px-4 max-w-75'>{resposta}</p>
-            </li>
-          ))}
-        </ul>
-        <div className="flex gap-1 w-full border-t border-solid border-slate-700 bg-slate-900 py-1 px-5 bottom-0">
-          <textarea placeholder='Envie uma mensagem...' className='bg-slate-900 border-none text-base resize-none py-4 pr-4 h-14 w-full outline-0 peer' value={mensagem} onChange={(e) => setMensagem(e.target.value)} required></textarea>
-          <AiOutlineSend className='text-brown-rust-600 text-2xl cursor-pointer self-end h-14 invisible peer-valid:visible' onClick={handleSendMensagem} />
+            {mensagensDiv.map((mensagemDiv, index) => (
+              <li key={index} className={`flex ${mensagemDiv.type === 'chatbot' ? '' : 'justify-end'} my-5`}>
+                {mensagemDiv.type === 'chatbot' ? <span className='h-8 w-8 text-center leading-8 mr-2 mb-2 self-end'>🤖</span> : ''}
+                <p className={`${mensagemDiv.type === 'chatbot' ? 'bg-slate-300' : 'bg-brown-rust-500'} ${mensagemDiv.type === 'chatbot' ? 'text-slate-950' : 'text-brown-rust-50'} rounded-lg py-3 px-4 max-w-75`}>
+                  {mensagemDiv.content}
+                </p>
+              </li>
+            ))}
+            {loading && (
+              <li className="flex">
+                <span className='h-8 w-8 text-center leading-8 mr-2 mb-2 self-end'>🤖</span>
+                <p className='bg-slate-300 text-slate-950 rounded-lg py-3 px-4 max-w-75'>Pensando...</p>
+              </li>
+            )}
+          </ul>
+          <div className="flex gap-1 w-full border-t border-solid border-slate-700 bg-slate-900 py-1 px-5 bottom-0 max-h40">
+            <textarea placeholder='Envie uma mensagem...' className='bg-slate-900 border-none text-base resize-none py-4 pr-4 h-14 w-full outline-0 peer' value={mensagem} onChange={(e) => setMensagem(e.target.value)} required></textarea>
+            <AiOutlineSend className='text-brown-rust-600 text-2xl cursor-pointer self-end h-14 invisible peer-valid:visible' onClick={handleSendMensagem} />
+          </div>
         </div>
       </div>
     </div>
